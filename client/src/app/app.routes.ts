e5997ecec72678367cfbff62431114f0e2592b3d
NGO HOANG GIANG <meobapgiang19062001@gmail.com>
2024-07-03 14:15:31 +0700

    push

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+export  const routes: Routes = [
+    { path: '', component: AppComponent },  // Default route
+    { path: 'about', component: AboutComponent }
+  ];
